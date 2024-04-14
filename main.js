6fd58858729ac5cfc14929aeef2d29f0b3a8946b
yunusisro <166944302+yunusisro@users.noreply.github.com>
2024-04-14 07:59:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express =require("express");
 const app=express();
 const PORT =  process.env.PORT ||3000;
 app.get ("/", (req, res) => {
- return res.json({ message:"hey, i am yunus"});
+ return res.json({ message:"hey, i am yunus and i automated a system"});
 }); 
 
 app.listen (PORT, () => console.log (`server started on PORT:${PORT}`))
